ff208c49970e7ec0606ff3e88bb2048bab40f23a
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-06-27 15:18:09 +0300

    Expose LogLevel

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { IAuthContext } from 'node-sp-auth-config';
 export { IAuthOptions } from 'node-sp-auth';
+export { LogLevel } from './utils/logger';
 export { Download, IDownloadOptions } from './api/Download';
